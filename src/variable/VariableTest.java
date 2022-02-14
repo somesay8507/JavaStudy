cfccc4435270b3407d12e327512a67f7e32f9087
somesay8507 <somesay8507@gmail.com>
2022-02-14 21:31:19 +0900

    Java - variable (정리중...)

---
+++ src/variable/VariableTest.java
@@ -0,0 +1,16 @@
+package variable;
+
+/* 변수
+ * 프로그램에서 사용되는 자료를 저장하기 위한 공간
+ * 할당 받은 메모리의 주소 대신 부르는 이름
+ * 프로그램 실행 중에 값 변경 가능
+ * 사용되기 이전에 선언 되어야 한다
+ */
+public class VariableTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
