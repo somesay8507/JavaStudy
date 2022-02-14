01d688603d869c9546393a0c14a6e09bb3de1618
somesay8507 <somesay8507@gmail.com>
2022-02-14 21:24:41 +0900

    Java - binary

---
+++ src/binary/BinaryTest2.java
@@ -0,0 +1,15 @@
+package binary;
+
+public class BinaryTest2 {
+
+	public static void main(String[] args) {
+		int num1 = 0B00000000000000000000000000000101; // 양수 5
+		int num2 = 0B11111111111111111111111111111011; // 음수 -5
+		
+		int sum = num1 + num2;
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(sum);
+		// 보수까지 알면 좋음...
+	}
+}
