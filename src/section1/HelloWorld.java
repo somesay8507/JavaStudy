347047c44fc7cc66238ca763123c1d85b0b957b1
somesay8507 <somesay8507@gmail.com>
2022-02-15 22:39:59 +0900

    End

--- src/section1/HelloWorld.java
+++ src/section1/HelloWorld.java
@@ -3,7 +3,7 @@ package section1;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("HelloWorld");
+		System.out.println("HelloWorld"); //첫 프로그램 만듬
 	}
 
 }
