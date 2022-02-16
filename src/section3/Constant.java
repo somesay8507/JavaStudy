8113c37bbf05c237e561bdcbac646e9332adf722
somesay8507 <somesay8507@gmail.com>
2022-02-16 22:12:52 +0900

    상수 형변환

---
+++ src/section3/Constant.java
@@ -0,0 +1,18 @@
+package section3;
+
+public class Constant {
+
+	public static void main(String[] args) {
+		final int MAX_NUM = 100;
+		final float PI = 3.14f;
+				
+		final int STUDENT_NUM = 31;
+		
+		int num = 0;
+		if ( num == STUDENT_NUM) {} 
+		
+		System.out.println(STUDENT_NUM);
+
+	}
+
+}
