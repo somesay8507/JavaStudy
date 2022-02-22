dde7381f4ff9fc3febba60bafad555871b87dfe9
somesay8507 <somesay8507@gmail.com>
2022-02-23 06:03:07 +0900

    관계 연산자 End

---
+++ src/section4/OperationEx4.java
@@ -0,0 +1,16 @@
+package section4;
+
+public class OperationEx4 {
+
+	public static void main(String[] args) {
+		
+		int num1 = 10;
+		System.out.println(num1 += 1);
+		
+		System.out.println(num1 %= 10);
+		num1 -= 1; //num1 = num1 - 1; 자기자신에 -1을 대입하라는 의미 
+		System.out.println(num1);
+
+	}
+
+}
