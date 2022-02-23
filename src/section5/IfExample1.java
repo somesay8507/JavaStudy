b13a68e5d5bcd7d0fc6d61d7f439c55c826c2ed0
somesay8507 <somesay8507@gmail.com>
2022-02-23 11:42:52 +0900

    조건문 if ~ else 이론정리

---
+++ src/section5/IfExample1.java
@@ -0,0 +1,17 @@
+package section5;
+
+public class IfExample1 {
+
+	public static void main(String[] args) {
+		
+		int age = 2;
+		
+		if(age >= 8) {
+			System.out.println("학교에 다닙니다.");
+		}else {
+			System.out.println("학교에 다니지 않습니다.");
+		}
+
+	}
+
+}
